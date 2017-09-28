09a2579d391dc6690c9ab5ce770c10439bae4363
Shawn <sdf1342@gmail.com>
2017-09-28 15:15:06 -0700

    add alhpa, and completed alpha

--- application/config/config.php
+++ application/config/config.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // the menu basics (text navbar)
 $config['menu_choices'] = array(
     'menudata' => array(
-        array('name' => 'Alpha', 'link' => '#'),
+        array('name' => 'Alpha', 'link' => 'able/index'),
         array('name' => 'Bravo', 'link' => '#'),
         array('name' => 'Charlie', 'link' => '#'),
-        array('name' => 'Delta', 'link' => '#'),
+        array('name' => 'Delta', 'link' => 'delta/force'),
         array('name' => 'Echo', 'link' => '#'),
         array('name' => 'Foxtrot', 'link' => '#'),
         array('name' => 'Golf', 'link' => '#'),
---
+++ application/controllers/able.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kimdongwon
+ * Date: 2017-09-28
+ * Time: 2:26 PM
+ */
+class Able extends Application{
+
+    public function index(){
+        $this->show(1);
+
+    }
+
+}
