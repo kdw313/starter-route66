f6d01fa518bd593480115b9ef7612be17dd3ae40
Shawn <sdf1342@gmail.com>
2017-09-28 18:50:54 -0700

    Delta completed

---
+++ application/controllers/delta/force.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kimdongwon
+ * Date: 2017-09-28
+ * Time: 6:37 PM
+ */
+
+class Force extends Application{
+
+    public function index(){
+        $this->show(4);
+    }
+
+}
