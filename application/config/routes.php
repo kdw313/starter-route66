f786e78370321b09327eb4b537a178a6b1a360dd
Shawn <sdf1342@gmail.com>
2017-09-29 13:15:04 -0700

    route.php routing issue for Hotel solved

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,11 +50,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['bananas/rule'] = function($fruit,$verb) {
-    return 'golf';
-};
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-//doesn't work
-//$route['i/(.*)'] = 'golf';x
 $route['i/(.*)'] = 'golf';
+
+$route['bananas/rule'] = function() {
+    return 'golf';
+};
