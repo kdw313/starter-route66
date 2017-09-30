037458c763d264e44a6b42b0df836f2bef84108e
lanceleih <lancelei.h@hotmail.ca>
2017-09-29 22:09:23 -0700

    India controller added, Capitan!

--- application/controllers/India.php
+++ application/controllers/India.php
@@ -12,21 +12,16 @@
  * @author Lancelei
  */
 class India extends CI_Controller{
-    function __construct()
-    {
-        parent::__construct();
-    }
-
     public function index (){
         // what will this reference?
-        $source = '../../public/data/logo.png'; 
+        $source = '../public/data/logo.png'; 
         // note that we could have referenced an image anywhere on our system
 
         // set the mime type for that image (jpeg, png, etc)
         header("Content-type: image/png"); 
         header('Content-Disposition: inline');
         readfile($source); // dish it
-
+        
     }
     
 }
